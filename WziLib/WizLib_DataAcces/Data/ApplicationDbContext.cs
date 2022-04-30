11c58fe5cf841cec2fe9c6c594361a3b42792775
Bartosz Sadowski <bshadowsky@gmail.com>
2022-04-30 13:42:11 +0200

    Added first model and first migration

--- WziLib/WizLib_DataAcces/Data/ApplicationDbContext.cs
+++ WziLib/WizLib_DataAcces/Data/ApplicationDbContext.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WizLib_Models.Models;
 
 namespace WizLib_DataAcces.Data
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
