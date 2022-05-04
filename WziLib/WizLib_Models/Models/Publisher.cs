f2f17bd6c041b7f0d245b8757ad10bf49dbbf7b0
Bartosz Sadowski <bshadowsky@gmail.com>
2022-05-04 20:46:42 +0200

    Added One to many relationship between book and publisher

--- WziLib/WizLib_Models/Models/Publisher.cs
+++ WziLib/WizLib_Models/Models/Publisher.cs
@@ -15,5 +15,6 @@ namespace WizLib_Models.Models
         public string Name { get; set; }
         [Required]
         public string Location { get; set; }
+        public List<Book> Books { get; set; }
     }
 }
