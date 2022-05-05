7af033c7adaf27ba0c54b844cbb24bd2ebc683a1
Bartosz Sadowski <bshadowsky@gmail.com>
2022-05-05 15:31:38 +0200

    Added Fluent classes

---
+++ WziLib/WizLib_Models/Models/Fluent_Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizLib_Models.Models
+{
+    public class Fluent_Book
+    {
+        
+        public int Book_Id { get; set; }
+        
+        public string Title { get; set; }
+        
+       
+        public string ISBN { get; set; }
+       
+        public double Price { get; set; }
+
+       
+        public int BookDetail_Id { get; set; }
+
+        public BookDetail BookDetail { get; set; }
+        
+
+        public int Publisher_Id { get; set; }
+
+        public Publisher Publisher { get; set; }
+
+        public ICollection<BookAuthor> BookAuthors { get; set; }
+
+    }
+}
---
+++ WziLib/WizLib_Models/Models/Fluent_BookAuthor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizLib_Models.Models
+{
+    public class Fluent_BookAuthor
+    {
+        
+        public int Book_Id { get; set; }
+        
+        public int Author_Id { get; set; }
+
+        public Book Book { get; set; }
+
+        public Author Author { get; set; }
+    }
+}
