2c28330ab0f925a1cdd997e081b47b3d9fafba79
Bartosz Sadowski <bshadowsky@gmail.com>
2022-05-06 01:05:46 +0200

    Added crud operations for Category

---
+++ WziLib/WizLib/Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WizLib_DataAcces.Data;
+using WizLib_Models.Models;
+
+namespace WizLib.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> model = _db.Categories.ToList();
+            return View(model);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category obj = new Category();
+            if(id == null)
+            {
+                return View(obj);
+            }
+            
+            obj = _db.Categories.FirstOrDefault(u=>u.Category_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            
+            return View(obj);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (obj.Category_Id==0)
+                {
+                    //creating data
+                    _db.Categories.Add(obj);
+                }
+                else
+                {
+                    //updating data
+                    _db.Categories.Update(obj); 
+                }
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = _db.Categories.FirstOrDefault(c => c.Category_Id == id);
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WziLib/WizLib_DataAcces/Migrations/20220505221945_AddRawCategoryTable.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WizLib_DataAcces.Migrations
+{
+    public partial class AddRawCategoryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Tbl_Category VALUES('Cat 1')");
+            migrationBuilder.Sql("INSERT INTO Tbl_Category VALUES('Cat 2')");
+            migrationBuilder.Sql("INSERT INTO Tbl_Category VALUES('Cat 3')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
